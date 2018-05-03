06f6694dd932f79476945f804bda74ab1a74be4a
kaktus30898 <linsi30898@gmail.com>
2018-05-03 21:10:56 +0700

    Переименовала Target в Shift (потому, что так не будет путаницы) и чуть-чуть его изменила

--- src/td/Components/Shift.java
+++ src/td/Components/Shift.java
@@ -4,19 +4,21 @@ import com.almasb.fxgl.entity.component.Component;
 import com.almasb.fxgl.entity.components.PositionComponent;
 import javafx.geometry.Point2D;
 
-public class Target extends Component {
+public class Shift extends Component {
     private PositionComponent position;
-    private Point2D shift;
+    private final Point2D shift;
 
-    public Target(Point2D shift) {
+    public Shift(Point2D shift) {
         this.shift = shift;
     }
 
-    public Target(int x, int y) {
-        this(new Point2D(x, y));
-    }
+    public Shift(double x, double y) { this(new Point2D(x, y)); }
+
+    public Shift(double radius) { this(radius, radius); }
 
     public Point2D getTarget() {
         return position.getValue().add(shift);
     }
+
+    public Point2D getShift() { return shift; }
 }
