0e59da8c156ffe7b1ef2007c5ddda52fff7b288b
kaktus30898 <linsi30898@gmail.com>
2018-05-03 21:12:27 +0700

    Новый класс - хранилище помощников

---
+++ src/td/Helpers/List.java
@@ -0,0 +1,23 @@
+package td.Helpers;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public class List {
+    public static <T> T getRandomElement(Collection<T> collection) {
+        if (collection.isEmpty())
+            return null;
+
+        final int index = (int) Math.floor(Math.random() * collection.size());
+
+        Iterator<T> it = collection.iterator();
+        for (int i = 0; i < index; i++) {
+            if (it.hasNext())
+                it.next();
+            else
+                it = collection.iterator();
+        }
+
+        return it.hasNext() ? it.next() : null;
+    }
+}
