88dc23f4599c9143a2d4a5a7ca2516179c563219
kaktus30898 <linsi30898@gmail.com>
2018-05-03 21:10:05 +0700

    У Hittable теперь можно подписаться на изменение перемеррой hitPoints

--- src/td/Components/Hittable.java
+++ src/td/Components/Hittable.java
@@ -1,11 +1,12 @@
 package td.Components;
 
 import com.almasb.fxgl.entity.component.Component;
+import javafx.beans.property.SimpleIntegerProperty;
 
 public class Hittable extends Component {
-    private int hitPoints = 10;
+    private final SimpleIntegerProperty hitPoints = new SimpleIntegerProperty(10);
     private boolean isAlive = true;
-    private Runnable onDead;
+    private final Runnable onDead;
 
     public Hittable(Runnable onDead) {
         this.onDead = onDead;
@@ -13,10 +14,14 @@ public class Hittable extends Component {
 
     public void hit(int decrement) {
         if (!isAlive) return;
-        hitPoints -= decrement;
-        if (hitPoints <= 0) {
+        final int newValue = hitPoints.get() - decrement;
+        hitPoints.set(newValue);
+        if (newValue <= 0) {
+            hitPoints.set(0);
             isAlive = false;
             onDead.run();
         }
     }
+
+    public SimpleIntegerProperty hitPoints() { return hitPoints; }
 }
